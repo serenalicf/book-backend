7cb2c8ac1f90c237a39770dd92b47db35437957c
serenali <aserena1103@gmail.com>
2024-09-21 20:56:56 +0800

    status code 201 for create book

--- src/main/java/com/inventory/management/book/controller/BookController.java
+++ src/main/java/com/inventory/management/book/controller/BookController.java
@@ -16,10 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import java.io.IOException;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
@@ -32,6 +34,7 @@ public class BookController {
     BookService bookService;
 
     @PostMapping("/books")
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@Valid @RequestBody CreateBookRequest createBookRequest) {
         Book book = bookService.createBook(createBookRequest);
         return BookMapper.INSTANCE.toDto(book);
