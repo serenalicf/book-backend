130bc99af8b4cd795fe3784e72a6d299ce74cefb
serenali <aserena1103@gmail.com>
2024-09-21 23:58:03 +0800

    remove unused import

--- src/main/java/com/inventory/management/book/mapper/BookMapper.java
+++ src/main/java/com/inventory/management/book/mapper/BookMapper.java
@@ -13,7 +13,6 @@ import org.mapstruct.factory.Mappers;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 @Mapper
 public interface BookMapper {
