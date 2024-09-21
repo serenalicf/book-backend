1d7971c0d54e86a18dc0181f0573a06987d72029
serenali <aserena1103@gmail.com>
2024-09-21 17:43:35 +0800

    remove unused import

--- src/main/java/com/inventory/management/book/exception/BookExceptionHandler.java
+++ src/main/java/com/inventory/management/book/exception/BookExceptionHandler.java
@@ -13,9 +13,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice
 public class BookExceptionHandler {
 
